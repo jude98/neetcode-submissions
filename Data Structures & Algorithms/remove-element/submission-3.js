e197c8c5876a3a180b55bbea1fd869cc22cf7b89
Boiled Beans <judithgeorgejoseph123@gmail.com>
2026-07-06 17:10:40 +0530

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let currentInsertPosition = 0;
+        for(let i = 0; i < nums.length; i++) {
+            if(nums[i] !== val) {
+                nums[currentInsertPosition++] = nums[i];
+            }
+        }
+
+        return currentInsertPosition
+
+    }
+}
