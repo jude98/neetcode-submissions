148f4c61c9664cd0f3a7f528136b50f84ebf8a3b
Boiled Beans <judithgeorgejoseph123@gmail.com>
2026-07-18 13:46:18 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        if (!height || height.length === 0) return 0;
+
+        let left = 0;
+        let right = height.length - 1;
+        
+        let leftMax = 0;
+        let rightMax = 0;
+        let totalWater = 0;
+
+        while (left < right) {
+            // The side with the lower current wall bottlenecks the water height
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left]; // Update max wall on the left
+                } else {
+                    totalWater += leftMax - height[left]; // Trap water column by column
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right]; // Update max wall on the right
+                } else {
+                    totalWater += rightMax - height[right]; // Trap water column by column
+                }
+                right--;
+            }
+        }
+
+        return totalWater;
+    }
+}
+
